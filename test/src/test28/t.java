2c1ac8994d7ab507873569d1927845d6670ecab8
Hikari <Hikari@Hikari>
2018-08-05 23:22:07 +0900

    z

--- test/src/test28/t.java
+++ test/src/test28/t.java
@@ -2,8 +2,26 @@ package test28;
 
 public class t {
 	public static void main(String[] args) {
-		String str = "";
+		int ranNum = 100;
+		int cnt = 0;
 		
+		for(int i=1;i<=ranNum;i++) {
+			int m = i/10;
+			int n = i%10;
+			
+			if(m!=0 && m%3==0) {
+				cnt++;
+				System.out.print("짝");
+			}
+			
+			if(n!=0 &&n%3==0) {
+				cnt++;
+				System.out.print("짝");
+			}
+			
+			System.out.println(i);
+		}
+		System.out.println("짝 갯수 " + cnt);
 	}
 
 }
---
+++ test/src/test28/te.java
@@ -0,0 +1,9 @@
+package test28;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class te {
+	
+
+}
